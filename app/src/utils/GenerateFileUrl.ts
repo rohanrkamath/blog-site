8a9341834e3410cd6d46c5c66a9b25905ceacd4a
Rohan Kamath <rohankamath@Rohans-MacBook-Pro.local>
2025-07-20 23:23:27 -0400

    Fix generateFileUrl to use correct /uploads/ endpoint

--- app/src/utils/GenerateFileUrl.ts
+++ app/src/utils/GenerateFileUrl.ts
@@ -6,6 +6,6 @@ import { UPLOAD_PATH_URL } from "@/config";
 
 const API_URL = process.env.NEXT_PUBLIC_API_URL || "http://127.0.0.1:4000";
 const generateFileUrl = (file: FileModel) =>
-  `${API_URL}/api/file/${file.path ? `${file.path}/` : ""}${file.filename}`;
+  `${API_URL}/uploads/${file.path ? `${file.path}/` : ""}${file.filename}`;
 
 export default generateFileUrl;
